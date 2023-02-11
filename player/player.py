42c6a7d7b463a0a57351b0a6f08650b72f4bc0f1
BachNob <tqbach184@gmail.com>
2023-02-11 21:26:21 +0700

    now it make even more sense

--- player/player.py
+++ player/player.py
@@ -10,21 +10,21 @@ class Cell(pygame.sprite.Sprite):
         self.color = this_frame[f"{self.cell[0]},{self.cell[1]}"]
         pygame.draw.rect(screen,self.drawcolor[self.color],self.rect)
 
-game_title = "badapple"
-screen_width = 900
-screen_height = 684
-game_speed = 1
+cell_row = 50
+cell_column = 38
+cell_width = 18
+
+window_title = "badapple"
+screen_width = cell_row*cell_width
+screen_height = cell_column*cell_width
+fps = 1
 
 pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption(game_title)
+pygame.display.set_caption(window_title)
 clock = pygame.time.Clock()
 font = pygame.font.Font("data/font.ttf",20)
-
-cell_row = 50
-cell_column = 38
-cell_width = 18
 
 color_black = (0,0,0)
 color_white = (255,255,255)
@@ -37,7 +37,7 @@ loading_text_rect = loading_text.get_rect()
 loading_text_rect.center = (screen_width/2,screen_height/2)
 
 frames = {}
-frame = 0
+frame_index = 0
 
 screen.blit(loading_text,loading_text_rect)
 pygame.display.flip()
@@ -61,15 +61,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    this_frame = frames[str(frame)]
+    this_frame = frames[str(frame_index)]
         
     screen.fill((0,0,0))
 
     cell_group.update()
 
-    frame += 1
+    frame_index += 1
 
     pygame.display.flip()
-    clock.tick(game_speed)        
+    clock.tick(fps)        
 
           
